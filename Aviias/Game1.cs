17d82ae7f3c5b164d638eec96c8027fb32948fe1
Quentin <barot@intechinfo.fr>
2017-04-21 19:46:16 +0200

    IA monster not finish

--- Aviias/Game1.cs
+++ Aviias/Game1.cs
@@ -37,8 +37,9 @@ namespace Aviias
             // TODO: Add your initialization logic here
 
             player = new Player();
-            monster = new Monster(100, 6.0f, 0.05, 10, 5 );
             playerMoveSpeed = 8.0f;
+            monster = new Monster(100, 1.0f, 0.05, 10, 5 );
+            
             
             base.Initialize();
         }
@@ -82,9 +83,10 @@ namespace Aviias
             // TODO: Add your update logic here
             previousKeyboardState = currentKeyboardState;
             currentKeyboardState = Keyboard.GetState();
-            UpdateMonster(gameTime);
-            UpdatePlayer(gameTime);
             
+            UpdatePlayer(gameTime);
+            monster.Update(player);
+            UpdateMonster(gameTime);
             base.Update(gameTime);
         }
 
