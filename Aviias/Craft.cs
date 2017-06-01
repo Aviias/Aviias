c9fc56fb287db77c3ea7c8e8deeda9a239dfcde8
Seb <martins@intechinfo.fr>
2017-06-01 10:53:33 +0200

    Added Craft

---
+++ Aviias/Craft.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aviias
+{
+    class Craft
+    {
+        Dictionary<int, int> _pelleEnBois;
+        Dictionary<int, int> _planche;
+        Dictionary<int, int> _stick;
+        Ressource _ressource;
+
+        public Craft()
+        {
+            _planche = new Dictionary<int, int>();
+            _stick = new Dictionary<int, int>();
+            _pelleEnBois = new Dictionary<int, int>();
+        }
+
+        public int Planche()
+        {
+            _planche.Add(1, _ressource.Id("bois"));
+            int Out = 4;
+            return Out;
+        }
+
+        public int Stick()
+        {
+            _stick.Add(2, _ressource.Id("planche"));
+            int Out = 4;
+            return Out;
+        }
+
+        public int PelleEnBois()
+        {
+            _pelleEnBois.Add(1, _ressource.Id("planche"));
+            _pelleEnBois.Add(2, _ressource.Id("stick"));
+            int Out = 1;
+            return Out;
+        }
+    }
+}
--- Aviias/Ressource.cs
+++ Aviias/Ressource.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Aviias
 {
-    class Ressource
+    public class Ressource
     {
+        Dictionary<int, string> _ressource;
+
+        public Ressource()
+        {
+            _ressource = new Dictionary<int, string>();
+            Add();
+        }
+
+        internal void Add()
+        {
+            _ressource.Add(1, "bois");
+            _ressource.Add(2, "pierre");
+            _ressource.Add(3, "terre");
+            _ressource.Add(4, "pelouse");
+            _ressource.Add(5, "planche");
+        }
+
+        public int Id(string ressource)
+        {
+            foreach (var i in _ressource)
+                {
+                if (i.Value == ressource) return i.Key;
+                else return -1;
+                }
+        }
     }
 }
